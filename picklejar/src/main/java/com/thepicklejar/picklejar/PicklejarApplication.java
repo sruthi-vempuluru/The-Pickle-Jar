6efa95e57c9a4d9fb8ccc2932002977005ac1e0d
Sruthi Vempuluru <sruthivempuluru@mac.lan>
2025-09-30 23:17:32 -0400

    Initial Spring Boot setup

---
+++ picklejar/src/main/java/com/thepicklejar/picklejar/PicklejarApplication.java
@@ -0,0 +1,13 @@
+package com.thepicklejar.picklejar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicklejarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicklejarApplication.class, args);
+	}
+
+}
