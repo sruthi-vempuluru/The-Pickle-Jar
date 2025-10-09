da1c019fbe4b68a78dacf7b32ce5464ba9e4415d
sruthi-vempuluru <53087346+sruthi-vempuluru@users.noreply.github.com>
2025-10-09 13:57:03 -0400

    Revert "Implement tournament backend services"

--- picklejar/src/main/java/com/thepicklejar/picklejar/repositories/TournamentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thepicklejar.picklejar.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.thepicklejar.picklejar.entities.Tournament;
-
-public interface TournamentRepository extends JpaRepository<Tournament, Long> {
-}
